8140ba99c5acf0258093764ba63e52fd0a1e7a03
MrN1ghtmoon <113134429+MrN1ghtmoon@users.noreply.github.com>
2024-03-09 13:13:58 +0200

    (task2) Creating variables and calculating the difference in costs with output.

--- code/index.php
+++ code/index.php
@@ -9,3 +9,21 @@ $order .= 'and 20 nuggets with cheese sauce';//concatenation
 
 //Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name";
+
+//Task 2
+$value = 1234;//integer
+echo "<br>";// "\n"
+echo $value;
+
+echo "<br>";
+
+$value2 = 15.555;//float
+echo $value2;
+
+echo "<br>";
+echo $value2 - 3.555;// output 12
+
+echo "<br>";
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "Разница в расходах - ".($last_month - $this_month);

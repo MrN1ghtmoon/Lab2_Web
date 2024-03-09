eb866f69c0b57363d9c26da110c15e51d124d7cd
MrN1ghtmoon <113134429+MrN1ghtmoon@users.noreply.github.com>
2024-03-09 12:51:29 +0200

    (task1) Add a reference to the variable and merge it with another string

--- code/index.php
+++ code/index.php
@@ -1,1 +1,11 @@
 <?php
+//Task 1
+/*Imagine a lot of code here*/
+$very_bad_unclear_name = "15 chicken wings";
+
+//Write your code here:
+$order = &$very_bad_unclear_name;
+$order .= 'and 20 nuggets with cheese sauce';//concatenation
+
+//Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name";
